0e395309d0dfc9373d7fada3c6fd515d8dd60f66
Karan Menon <karanmenon@karans-mbp-2.lan>
2021-06-13 20:40:13 -0400

    Started code

--- scheduling.java
+++ scheduling.java
@@ -3,10 +3,22 @@ import java.io.*;
 
 public class Scheduling
 {
+    String[] session1Classes;
+    String[] session2Classes;
+    HashMap<String, ArrayList<>> session1Enrollment;
 
+    public Scheduling()
+    {
+        session1Classes= new String[]{"Myth and Religion", "Prose", "US History", "Chemistry", "Geography", "Poetry", "European History",
+    "Biology", "Visual Art", "Drama", "World History", "Physics", "Music"};
+        session2Classes= new String[]{"Prose II", "Politics", "Myth and Religion", "Biology II", "Poetry II", "Military History",
+    "Philosophy & Society", "Chemistry II", "Authors", "Monarchy", "Geography II", "Earth and Space", "Music II"};
+        
+
+    }
     public static void main(String [] args)
     {
-        
+
     }
 }
     
