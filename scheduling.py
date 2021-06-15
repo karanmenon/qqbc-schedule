bae0c9294e2e69169153cb50378d76c1bdb82806
Karan Menon <karanmenon@karans-mbp-2.lan>
2021-06-14 21:21:57 -0400

    more api stuff

--- scheduling.py
+++ scheduling.py
@@ -16,8 +16,8 @@ scope=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth
 credentials= ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 client=gspread.authorize(credentials)
 sheet=client.open('https://docs.google.com/spreadsheets/d/1Ev3R1HH_eNRTxhoy8a5dxAG0Bkk85-Ef1kpUK63UVUs/edit?usp=sharing')
-campers_data=sheet.get_worksheet(1).get_all_records()
-instructors_data=sheet.get_worksheet(2).get_all_records()
+campers_data=pd.DataFrame.from_dict(sheet.get_worksheet(1).get_all_records())
+instructors_data=pd.DataFrame.from_dict(sheet.get_worksheet(2).get_all_records())
 
 session1Classes=["Myth and Religion", "Prose", "US History", "Chemistry", "Geography", "Poetry", "European History",
     "Biology", "Visual Art", "Drama", "World History", "Physics", "Music"]
@@ -25,6 +25,16 @@ session2Classes= ["Prose II", "Politics", "Myth and Religion II", "Biology II", 
     "Philosophy and Society", "Chemistry II", "Authors", "Monarchy", "Geography II", "Earth and Space", "Music II"]
 
 students={}
+for ind in campers_data.index:
+    preference_list=[]
+    preference_list.append(campers_data['Class preference: [1]'][ind])
+    preference_list.append(campers_data['Class preference: [2]'][ind])
+    preference_list.append(campers_data['Class preference: [3]'][ind])
+    preference_list.append(campers_data['Class preference: [4]'][ind])
+    preference_list.append(campers_data['Class preference: [5]'][ind])
+
+    s=Student(campers_data['Name'][ind], campers_data['Session 1 or 2'][ind], campers_data['QB Exp.'][ind], preference_list)
+    students.append(s)
 classEnrollment={}
 studentSchedule={}
 
@@ -32,7 +42,7 @@ service=discovery.build('sheets', 'v4', credentials=credentials)
 spreadsheet_id="1Ev3R1HH_eNRTxhoy8a5dxAG0Bkk85-Ef1kpUK63UVUs"
 campers_sheet_id=2008186473
 instructors_sheet_id=1244594342
-for str in session1Classes:
+#for str in session1Classes:
 
-for str in session2Classes:
+#for str in session2Classes:
 
