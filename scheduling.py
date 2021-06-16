6f734462d77bfdb6a4eb89bc6347fbb52b114e6d
Karan Menon <karanmenon@karans-mbp-2.lan>
2021-06-16 18:32:15 -0400

    more data structures

--- scheduling.py
+++ scheduling.py
@@ -5,12 +5,28 @@ import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
+session_num=1
 class Student:
     def __init__(self, name, session, qb_exp, preference):
         self.name=name
         self.session=session
         self.qb_exp=qb_exp
         self.preference=preference
+        self.classes=[]
+class ClassSections:
+    def __init__(self, name, teacher, enrollment, max_priority):
+        self.name=name
+        self.teacher=teacher
+        self.enrollment=enrollment
+        self.max_priority=max_priority
+        self.capacity=10
+classEnrollment=[[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()], 
+[ClassSections(),ClassSections(),ClassSections(),ClassSections()]]
 
 scope=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']        
 credentials= ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
@@ -23,55 +39,7 @@ session1Classes=["Myth and Religion", "Prose", "US History", "Chemistry", "Geogr
     "Biology", "Visual Art", "Drama", "World History", "Physics", "Music"]
 session2Classes= ["Prose II", "Politics", "Myth and Religion II", "Biology II", "Poetry II", "Military History",
     "Philosophy and Society", "Chemistry II", "Authors", "Monarchy", "Geography II", "Earth and Space", "Music II"]
-mythReligion=[]
-prose=[]
-usHistory=[]
-chemistry=[]
-geography=[]
-poetry=[]
-euroHist=[]
-biology=[]
-visualArt=[]
-drama=[]
-worldHist=[]
-physics=[]
-music=[]
-prose2=[]
-politics=[]
-mythReligion2=[]
-biology2=[]
-militaryHist=[]
-philSociety=[]
-chemistry2=[]
-authors=[]
-monarchy=[]
-geography2=[]
-earthSpace=[]
-music2=[]
 
-def getClassList(className):
-    
-    print(x)
-
-students={}
-for ind in campers_data.index:
-    preference_list=[]
-    preference_list.append(campers_data['Class preference: [1]'][ind])
-    preference_list.append(campers_data['Class preference: [2]'][ind])
-    preference_list.append(campers_data['Class preference: [3]'][ind])
-    preference_list.append(campers_data['Class preference: [4]'][ind])
-    preference_list.append(campers_data['Class preference: [5]'][ind])
-
-    s=Student(campers_data['Name'][ind], campers_data['Session 1 or 2'][ind], campers_data['QB Exp.'][ind], preference_list)
-    students.append(s)
-classEnrollment={}
-studentSchedule={}
-
-service=discovery.build('sheets', 'v4', credentials=credentials)
-spreadsheet_id="1Ev3R1HH_eNRTxhoy8a5dxAG0Bkk85-Ef1kpUK63UVUs"
-campers_sheet_id=2008186473
-instructors_sheet_id=1244594342
-#for str in session1Classes:
 classPeriods={
     "Geography": [2,3,4],
     "Prose": [2],
@@ -99,5 +67,29 @@ classPeriods={
     "Earth and Space":[4],
     "Music II": [4],
 }
+
+strategy=[]
+howToQB=[]
+session1Students=[]
+session2Students=[]
+for ind in campers_data.index:
+    preference_list=[]
+    preference_list.append(campers_data['Class preference: [1]'][ind])
+    preference_list.append(campers_data['Class preference: [2]'][ind])
+    preference_list.append(campers_data['Class preference: [3]'][ind])
+    preference_list.append(campers_data['Class preference: [4]'][ind])
+    preference_list.append(campers_data['Class preference: [5]'][ind])
+
+    s=Student(campers_data['Name'][ind], campers_data['Session 1 or 2'][ind], campers_data['QB Exp.'][ind], preference_list)
+    students.append(s)
+classEnrollment={}
+studentSchedule={}
+
+service=discovery.build('sheets', 'v4', credentials=credentials)
+spreadsheet_id="1Ev3R1HH_eNRTxhoy8a5dxAG0Bkk85-Ef1kpUK63UVUs"
+campers_sheet_id=2008186473
+instructors_sheet_id=1244594342
+#for str in session1Classes:
+
 #for str in session2Classes:
 
